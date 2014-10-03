1dcc3eca33be6f9949a4e433e9cd2ae922de7580
FrostByte <lewisscott0@gmail.com>
2014-10-03 11:45:54 -0700

    ASP.net MVC-5 Boilerplate

---
+++ 3900 Civionics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3900_Civionics.Startup))]
+namespace _3900_Civionics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
