36ff0769773595c2394242c99075b2ba4b03d403
FrostByte <lewisscott0@gmail.com>
2014-10-17 15:59:41 -0700

    Created projects model and controller
    Creates, deletes and lists projects. Also added boilerplate for project
    access table. Only admin has access to the create and delete. Users that
    are not admin can only list projects that they have access to.

---
+++ 3900 Civionics/Models/ExtendedAccountModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _3900_Civionics.Models
+{
+    public class AccountInfo
+    {
+        public string Username { get; set; }
+        public string Organization { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class AccountInfoDBContext : DbContext
+    {
+        public DbSet<AccountInfo> AccountInfoList { get; set; }
+    }
+}
---
+++ 3900 Civionics/Models/ProjectModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace _3900_Civionics.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+
+    }
+
+    public class ProjectDBContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+    }
+
+    public class ProjectAccess
+    {
+        public int ProjectID { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class ProjectAccessDBContext : DbContext
+    {
+        public DbSet<ProjectAccess> ProjectAccessList{ get; set; }
+    }
+}
