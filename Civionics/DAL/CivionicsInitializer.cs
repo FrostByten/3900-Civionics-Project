ad985f5b436185dde19a08f7d5a95a2f1efc05c1
MrChimick <mikechimick@gmail.com>
2014-11-01 18:27:00 -0700

    unit and type tables

--- Civionics/DAL/CivionicsInitializer.cs
+++ Civionics/DAL/CivionicsInitializer.cs
@@ -12,6 +12,25 @@ namespace Civionics.DAL
     {
         protected override void Seed(CivionicsContext context)
         {
+            var units = new List<Unit>
+            {
+                new Unit{UnitID="Gs"},
+                new Unit{UnitID="Volts"},
+                new Unit{UnitID="Celcius"}
+            };
+            units.ForEach(p => context.Units.Add(p));
+            context.SaveChanges();
+
+            var types = new List<SensorType>
+            {
+                new SensorType{SensorTypeID="X-Axis Accelerometer"},
+                new SensorType{SensorTypeID="Y-Axis Accelerometer"},
+                new SensorType{SensorTypeID="Z-Axis Accelerometer"},
+                new SensorType{SensorTypeID="Corrosion"}
+            };
+            units.ForEach(p => context.Units.Add(p));
+            context.SaveChanges();
+
             var projects = new List<Project>
             {
                 new Project{ID=0,Name="TestProject1",Description="Test project 1", DateAdded=DateTime.Parse("2014-10-31")},
@@ -25,26 +44,26 @@ namespace Civionics.DAL
 
             var sensors = new List<Sensor>
             {
-                new Sensor{SensorID=0,ProjectID=0,Name="TestSensor1",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=1,MaxSafeReading=5},
-                new Sensor{SensorID=1,ProjectID=0,Name="TestSensor2",Type="3-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=0,MaxSafeReading=40},
-                new Sensor{SensorID=2,ProjectID=0,Name="TestSensor3",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=5,MaxSafeReading=10},
-                new Sensor{SensorID=3,ProjectID=0,Name="TestSensor4",Type="2-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=6,MaxSafeReading=11},
-                new Sensor{SensorID=4,ProjectID=1,Name="TestSensor5",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=0,MaxSafeReading=2},
-                new Sensor{SensorID=5,ProjectID=1,Name="TestSensor6",Type="3-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=20,MaxSafeReading=60},
-                new Sensor{SensorID=6,ProjectID=1,Name="TestSensor7",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=8,MaxSafeReading=400},
-                new Sensor{SensorID=7,ProjectID=1,Name="TestSensor8",Type="2-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=1,MaxSafeReading=6},
-                new Sensor{SensorID=8,ProjectID=2,Name="TestSensor9",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=0,MaxSafeReading=1},
-                new Sensor{SensorID=9,ProjectID=2,Name="TestSensor10",Type="Corrosion",Location="Here",UnitOfMeasure="Degrees C",MinSafeReading=-2,MaxSafeReading=69},
-                new Sensor{SensorID=10,ProjectID=2,Name="TestSensor11",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=3,MaxSafeReading=4},
-                new Sensor{SensorID=11,ProjectID=2,Name="TestSensor12",Type="2-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=-6,MaxSafeReading=3},
-                new Sensor{SensorID=12,ProjectID=3,Name="TestSensor13",Type="Corriosion",Location="Here",UnitOfMeasure="Degrees F",MinSafeReading=-40,MaxSafeReading=200},
-                new Sensor{SensorID=13,ProjectID=3,Name="TestSensor14",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=2,MaxSafeReading=4},
-                new Sensor{SensorID=14,ProjectID=3,Name="TestSensor15",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=0,MaxSafeReading=45},
-                new Sensor{SensorID=15,ProjectID=3,Name="TestSensor16",Type="3-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=-20,MaxSafeReading=74},
-                new Sensor{SensorID=16,ProjectID=4,Name="TestSensor17",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=1,MaxSafeReading=8},
-                new Sensor{SensorID=17,ProjectID=4,Name="TestSensor18",Type="1-Axis Accelerometer",Location="Here",UnitOfMeasure="Gs",MinSafeReading=6,MaxSafeReading=10},
-                new Sensor{SensorID=18,ProjectID=4,Name="TestSensor19",Type="Corrosion",Location="Here",UnitOfMeasure="Degrees F",MinSafeReading=-200,MaxSafeReading=102},
-                new Sensor{SensorID=19,ProjectID=4,Name="TestSensor20",Type="Corrosion",Location="Here",UnitOfMeasure="Degrees C",MinSafeReading=-2,MaxSafeReading=55}
+                new Sensor{ID=0,ProjectID=0,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=1,MaxSafeReading=5},
+                new Sensor{ID=1,ProjectID=0,SensorTypeID="Y-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=0,MaxSafeReading=40},
+                new Sensor{ID=2,ProjectID=0,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=5,MaxSafeReading=10},
+                new Sensor{ID=3,ProjectID=0,SensorTypeID="X-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=6,MaxSafeReading=11},
+                new Sensor{ID=4,ProjectID=1,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=0,MaxSafeReading=2},
+                new Sensor{ID=5,ProjectID=1,SensorTypeID="Y-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=20,MaxSafeReading=60},
+                new Sensor{ID=6,ProjectID=1,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=8,MaxSafeReading=400},
+                new Sensor{ID=7,ProjectID=1,SensorTypeID="X-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=1,MaxSafeReading=6},
+                new Sensor{ID=8,ProjectID=2,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=0,MaxSafeReading=1},
+                new Sensor{ID=9,ProjectID=2,SensorTypeID="Corrosion",SiteID="Here",UnitID="Degrees C",MinSafeReading=-2,MaxSafeReading=69},
+                new Sensor{ID=10,ProjectID=2,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=3,MaxSafeReading=4},
+                new Sensor{ID=11,ProjectID=2,SensorTypeID="X-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=-6,MaxSafeReading=3},
+                new Sensor{ID=12,ProjectID=3,SensorTypeID="Corriosion",SiteID="Here",UnitID="Degrees F",MinSafeReading=-40,MaxSafeReading=200},
+                new Sensor{ID=13,ProjectID=3,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=2,MaxSafeReading=4},
+                new Sensor{ID=14,ProjectID=3,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=0,MaxSafeReading=45},
+                new Sensor{ID=15,ProjectID=3,SensorTypeID="Y-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=-20,MaxSafeReading=74},
+                new Sensor{ID=16,ProjectID=4,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=1,MaxSafeReading=8},
+                new Sensor{ID=17,ProjectID=4,SensorTypeID="Z-Axis Accelerometer",SiteID="Here",UnitID="Gs",MinSafeReading=6,MaxSafeReading=10},
+                new Sensor{ID=18,ProjectID=4,SensorTypeID="Corrosion",SiteID="Here",UnitID="Degrees F",MinSafeReading=-200,MaxSafeReading=102},
+                new Sensor{ID=19,ProjectID=4,SensorTypeID="Corrosion",SiteID="Here",UnitID="Degrees C",MinSafeReading=-2,MaxSafeReading=55}
             };
             sensors.ForEach(s => context.Sensors.Add(s));
             context.SaveChanges();
