e2889daa818d3d630a7b9073d85f18a9c57b95e1
slee-colonel <slee_colonel@hotmail.com>
2014-11-22 19:33:14 -0800

    Charting using DataVisualization.Charting complete
    Also added some comments and changed some webpage wordings

--- Civionics/Controllers/ReadingController.cs
+++ Civionics/Controllers/ReadingController.cs
@@ -9,6 +9,9 @@ using System.Net;
 using Civionics.Models;
 using Civionics.DAL;
 using System.Web.Helpers;
+using System.IO;
+using System.Web.UI.DataVisualization.Charting;
+using System.Drawing;
 
 namespace Civionics.Controllers
 {
@@ -94,36 +97,92 @@ namespace Civionics.Controllers
 
         // GET: /Reading/Chart
         /// <summary>
-        /// Gives a given number of readings in order for a given sensor
+        /// Renders the graph of a sensor's data
         /// </summary>
         /// <param name="id">The id of the sensor to get readings for</param>
-        /// <param name="num">The number of readings to get</param>
-        /// <returns>The View filled with readings from the sensor</returns>
+        /// <param name="num">The number of latest readings to get</param>
+        /// <returns>Returns the graph in png format</returns>
         public ActionResult Chart(int? id, int? num)
         {
             if (id == null || num == null)
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            
+
             Sensor s = db.Sensors.Find(id);
 
             if (s == null)
                 return HttpNotFound();
 
-            ViewData.Add("units", s.Type.Units);
-            ViewData.Add("min", s.MinSafeReading);
-            ViewData.Add("max", s.MaxSafeReading);
+            System.Web.UI.DataVisualization.Charting.Chart chart =
+                new System.Web.UI.DataVisualization.Charting.Chart();
 
-            List<Reading> list = db.Readings.Where(k => k.SensorID == id).OrderByDescending(k => k.ID).ToList();
-            if(list.Count < num)
-                return View(list.OrderBy(k => k.LoggedTime));
-            else
+            chart.Width = 1400;
+            chart.Height = 800;
+            chart.ChartAreas.Add(new ChartArea());
+
+            // label the axes
+            chart.ChartAreas[0].AxisX.Title = "Time";
+            chart.ChartAreas[0].AxisX.TitleFont = new Font("Arial", 12f, FontStyle.Bold);
+            chart.ChartAreas[0].AxisY.Title = s.Type.Type + " (" + s.Type.Units + ")";
+            chart.ChartAreas[0].AxisY.TitleFont = new Font("Arial", 12f, FontStyle.Bold);
+
+            for (int i = 0; i < 4; i++)
+                chart.Series.Add(new Series());
+
+            // declarations and stylings for lines and points
+            // main data line
+            chart.Series[0].ChartType = SeriesChartType.Line;
+            chart.Series[0].Color = Color.Navy;
+            chart.Series[0].BorderWidth = 2;
+            // upper limit line
+            chart.Series[1].ChartType = SeriesChartType.Line;
+            chart.Series[1].Color = Color.DarkRed;
+            chart.Series[1].BorderWidth = 3;
+            chart.Series[1].BorderDashStyle = ChartDashStyle.Dash;
+            // lower limit line
+            chart.Series[2].ChartType = SeriesChartType.Line;
+            chart.Series[2].Color = Color.DarkRed;
+            chart.Series[2].BorderWidth = 3;
+            chart.Series[2].BorderDashStyle = ChartDashStyle.Dash;
+            // data point highlight
+            chart.Series[3].ChartType = SeriesChartType.Point;
+            chart.Series[3].MarkerSize = 10;
+            //chart.Series[5].ChartType = SeriesChartType.Bar; // time period division
+
+            // set the graph data, and only get the specified number of most recent points
+            List<Reading> list = db.Readings.Where(k => k.SensorID == id).OrderBy(k => k.ID).ToList();
+
+            // no data == no image
+            if (list.Count == 0)
+                return null;
+
+            for (int i = (int)num - 1; i >=0; i--)
             {
-                List<Reading> o = new List<Reading>();
-                for(int i = 0; i < num; i++)
-                {
-                    o.Add(list[i]);
-                }
-                return View(o.OrderBy(k => k.LoggedTime));
+                string x = list[list.Count - i - 1].LoggedTime.ToString();
+                double y = list[list.Count - i - 1].Data;
+
+                chart.Series[0].Points.AddXY(x, y); // data line
+                chart.Series[1].Points.AddXY(x, s.MaxSafeReading); // upper limit line
+                chart.Series[2].Points.AddXY(x, s.MinSafeReading); // lower limit line
+                chart.Series[3].Points.AddXY(x, y); // data points
+
+                if (y < s.MinSafeReading || y > s.MaxSafeReading)
+                    chart.Series[3].Points[(int)num - i - 1].Color = Color.Red;
+                else
+                    chart.Series[3].Points[(int)num - i - 1].Color = Color.Aqua;
+            }
+
+            // labelling the max and min lines
+            chart.Series[1].Points[0].Label = "Maximum Acceptable Value";
+            chart.Series[1].Points[0].LabelForeColor = Color.DarkRed;
+            chart.Series[2].Points[0].Label = "Minimum Acceptable Value";
+            chart.Series[2].Points[0].LabelForeColor = Color.DarkRed;
+
+            // draw the graph out to a file
+            using (var ms = new MemoryStream())
+            {
+                chart.SaveImage(ms, ChartImageFormat.Png);
+                ms.Seek(0, SeekOrigin.Begin);
+                return File(ms.ToArray(), "image/png", "datachart.png");
             }
         }
 
